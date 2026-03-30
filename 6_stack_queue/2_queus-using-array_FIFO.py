4a81304e010aea5d7b78dec64593e47e0dc486a4
Amol magdum <amolmagdum25@yahoo.in>
2026-03-30 08:25:53 +0000

    adding queue code

---
+++ 6_stack_queue/2_queus-using-array_FIFO.py
@@ -0,0 +1,29 @@
+class Queue{
+    def __init__(self):
+        self.items[]
+
+    def is_empty(self):
+        return len (self.items) == 0
+
+    def enqueue(self,item):
+        self.item.append(item)
+    
+    def dequeue(self):
+        if len(self.items) == 0
+            print("queue is empty")
+            return
+        x = self.items.pop(0)
+        return x
+
+    def front(self):
+        if len(self.items) == 0
+            print("queue is empty")
+            return
+        return self.items[0]
+
+    def rear(self):
+        if len(self.items) == 0
+            print("queue is empty")
+            return
+        return self.items[-1]
+}
