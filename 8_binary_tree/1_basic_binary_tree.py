140a14dc48ffa9e345c7eed046b35d254f5851fe
Amol magdum <amolmagdum25@yahoo.in>
2026-04-29 06:15:01 +0000

    binary tree started

---
+++ 8_binary_tree/1_basic_binary_tree.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+drinks = Node("drinks")
+hot = Node("hot")
+cold = Node("cold")
+tea = Node("tea")
+coffee = Node("coffee")
+fanta = Node("fanta")
+cola = Node("fanta")
+
+hot.left = tea
+hot.right = coffee
+
+cold.left = cola
+cold.right = fanta
+
+drinks.left = hot
+drinks.right = cold
+
+print(drinks.val)
+print(drinks.left.val)
+print(drinks.right.left.val)
